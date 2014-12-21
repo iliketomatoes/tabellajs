c6c5e5cc49d661c21e7a44876484b0ce0d1d1637
iliketomatoes <giancarlosoverini@gmail.com>
2014-12-21 19:32:24 +0100

    Update test

--- test/tabella_test.js
+++ test/tabella_test.js
@@ -3,38 +3,33 @@ var tabella = document.getElementById("tabella");
 
 describe("Tabella", function() {
   describe("constructor", function() {
-  	it("should return a TabellaException when no element is passed to the constructor", function() {
+  	it("should throw a TabellaException when no element is passed to the constructor", function() {
 
-  		var test = new Tabella();
-    	expect(test).to.be.an.instanceof(TabellaException);
+    	expect(function(){ new Tabella(); }).to.throw(TabellaException);
     });
-    it("should return a TabellaException when options are empty", function() {
+    it("should throw a TabellaException when options are empty", function() {
 
-  		var test = new Tabella(tabella);
-    	expect(test).to.be.an.instanceof(TabellaException);
+    	expect(function(){ new Tabella(tabella); }).to.throw(TabellaException);
     });
-    it("should return a TabellaException when rows or periods are null", function() {
+    it("should throw a TabellaException when rows or periods are null", function() {
 
-  		var test = new Tabella(tabella,{});
-    	expect(test).to.be.an.instanceof(TabellaException);
+    	expect(function(){ new Tabella(tabella,{}); }).to.throw(TabellaException);
     });
-    it("should return a TabellaException when no rows are passed into the options", function() {
+    it("should throw a TabellaException when no rows are passed into the options", function() {
 
-  		var test = new Tabella(tabella,{ periods : [
+    	expect(function(){ new Tabella(tabella,{ periods : [
 
-  											['x','y']
+                        ['x','y']
 
-  										]});
-    	expect(test).to.be.an.instanceof(TabellaException);
+                      ]}); }).to.throw(TabellaException);
     });
-    it("should return a TabellaException when no periods are passed into the options", function() {
+    it("should throw a TabellaException when no periods are passed into the options", function() {
 
-  		var test = new Tabella(tabella,{ rows : [
+    	expect(function(){ new Tabella(tabella,{ rows : [
 
-  											['x','y']
+                        ['x','y']
 
-  										]});
-    	expect(test).to.be.an.instanceof(TabellaException);
+                      ]}); }).to.throw(TabellaException);
     });
   });
 
