eed58863c7443609668007a4c8a603e036ad028c
iliketomatoes <giancarlosoverini@gmail.com>
2015-01-16 10:27:58 +0100

    Update touch handler

--- src/toucher.js
+++ src/toucher.js
@@ -69,6 +69,9 @@
 			self.points.currX = pointer.pageX;
 			self.points.currY = pointer.pageY;
 
+			//We just want horizontal movements
+			if(Math.abs(self.points.cachedY - self.points.currY) >= Math.abs(self.points.cachedX - self.points.currX)) return false;
+		
 			return self.points;
 		}
 	};
